77f08775f7d1cba4136701df7e277bb6c8cc3c32
Blake <80996688+BlakeTnr@users.noreply.github.com>
2024-08-08 19:43:20 -0400

    Add tagging utilities for tracking and stopping

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,10 @@ const port = 3000
 
 var ec2_region = new EC2({region: 'us-east-1', maxAttempts: 15});
 
+var ec2Instances = [
+    "i-0a87aa86329a56c68"
+]
+
 function startInstanceById(id) {
     const input = { // StartInstancesRequest
         InstanceIds: [ // InstanceIdStringList // required
@@ -29,10 +33,75 @@ function stopInstanceById(id) {
     })
 }
 
+function tagEC2Instance(id, key, value) {
+    var params = {
+        Resources: [
+           id
+        ], 
+        Tags: [
+           {
+          Key: key, 
+          Value: value
+         }
+        ]
+       };
+    ec2_region.createTags(params)
+}
+
+function untagEC2Instance(id, key) {
+    var params = {
+        Resources: [
+           id
+        ], 
+        Tags: [
+           {
+          Key: key,
+         }
+        ]
+       };
+    ec2_region.deleteTags(params)
+}
+
+function getEC2TagValue(id, key) {
+    ec2_region.describeTags().then((tags) => {
+        tags.Tags.forEach((tag) => {
+            if(tag.Key == key) {
+                return tag.Value
+            }
+        })
+    })
+}
+
+function tagEC2StopAt(id, stopat) {
+    tagEC2Instance(id, "StopAt", stopat)
+}
+
+function tagEC2DelayedStop(id, secondsDelay) {
+    tagEC2StopAt(id, Math.floor(new Date().getTime()/1000)+secondsDelay)
+}
+
+function isEC2PastStopAt(id) {
+    var stopAtUNIX = getEC2TagValue(id, "StopAt")
+    if(new Date().getTime() > stopAtUNIX) {
+        return true
+    }
+    return false
+}
+
+function checkForPendingStopEC2() {
+    ec2Instances.forEach((ec2Instance) => {
+        if(isEC2PastStopAt(ec2Instance)) {
+            stopInstanceById(ec2Instance)
+            untagEC2Instance(ec2Instance, "StopAt")
+        }
+    })
+}
+
 const my_test_id = "i-0a87aa86329a56c68" // TODO: Remove this
 app.get('/', (req, res) => {
     res.send("AWSMCStart backend API")
     startInstanceById(my_test_id)
+    tagEC2DelayedStop(my_test_id, 20)
 })
 
 app.listen(port, () => {
