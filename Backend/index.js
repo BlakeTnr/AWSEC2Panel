1278faf7a71bf2b87551099c3a1b0e61b5b3cfbd
Blake <80996688+BlakeTnr@users.noreply.github.com>
2024-08-09 21:38:12 -0400

    Move backend code out of frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,157 +1,55 @@
-const { EC2, StartInstancesCommand, EC2Client } = require('@aws-sdk/client-ec2');
-const express = require('express')
-const app = express()
-const port = 3000
-var cron = require('node-cron');
-
-var ec2_region = new EC2({region: 'us-east-1', maxAttempts: 15}); // TODO: Turn the region into an environment variable
-
-var ec2Instances = [
-    "i-0a87aa86329a56c68" // TODO: Turn the instances into an environment variable
-]
-
-function startInstanceById(id) {
-    console.log(`Sending start for ${id}...`)
-    const input = { // StartInstancesRequest
-        InstanceIds: [ // InstanceIdStringList // required
-            id,
-        ]
-    };
-    ec2_region.startInstances(input, (err, data) => {
-        //if(err) console.log(err, err.stack)
-        //else console.log(data)
-    })
-    console.log(`Sent start for ${id}!`)
-}
-
-function stopInstanceById(id) {
-    const input = { // StartInstancesRequest
-        InstanceIds: [ // InstanceIdStringList // required
-            id,
-        ]
-    };
-    ec2_region.stopInstances(input, (err, data) => {
-        //if(err) console.log(err, err.stack)
-        //else console.log(data)
-    })
-}
-
-function tagEC2Instance(id, key, value) {
-    var params = {
-        Resources: [
-           id
-        ], 
-        Tags: [
-           {
-          Key: key, 
-          Value: value
-         }
-        ]
-       };
-    ec2_region.createTags(params)
-}
-
-function untagEC2Instance(id, key) {
-    var params = {
-        Resources: [
-           id
-        ], 
-        Tags: [
-           {
-          Key: key,
-         }
-        ]
-       };
-    ec2_region.deleteTags(params)
-}
-
-async function getEC2TagValue(id, key) {
-    var tags = await ec2_region.describeTags()
-    var result = undefined
-    tags.Tags.forEach((tag) => {
-        if(tag.Key == key) {
-            result = tag.Value
-        }
-    })
-    return result
-}
-
-async function getEC2Name(id) {
-    var result = null;
-    var instances = await ec2_region.describeInstances()
-    instances.Reservations.forEach((reservation) => {
-        reservation.Instances.forEach((instance) => {
-            if(instance.InstanceId == id) {
-                result = instance.KeyName
-            }
-        })
-    })
-    return result;
-}
-
-function tagEC2StopAt(id, stopat) {
-    tagEC2Instance(id, "StopAt", stopat)
-}
-
-function tagEC2DelayedStop(id, secondsDelay) {
-    tagEC2StopAt(id, Math.floor(new Date().getTime()/1000)+secondsDelay)
-}
-
-async function isEC2PastStopAt(id) {
-    var stopAtUNIX = await getEC2TagValue(id, "StopAt")
-    if(stopAtUNIX != null && Math.floor(new Date().getTime()/1000) > stopAtUNIX) {
-        return true
-    }
-    return false
-}
-
-async function checkForPendingStopEC2() {
-    ec2Instances.forEach(async (ec2Instance) => {
-        if(await isEC2PastStopAt(ec2Instance)) {
-            console.log(`Stopping ${ec2Instance}...`)
-            stopInstanceById(ec2Instance)
-            untagEC2Instance(ec2Instance, "StopAt")
-            console.log(`Sent stop for ${ec2Instance}!`)
-        }
-    })
-}
-
-function startEC2StopDaemon() {
-    cron.schedule('* * * * * *', async () => {
-        await checkForPendingStopEC2()
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-}
-
-const my_test_id = "i-0a87aa86329a56c68" // TODO: Remove this
-app.get('/', (req, res) => {
-    res.send("AWSMCStart backend API")
-    tagEC2DelayedStop(my_test_id, 60)
-    startInstanceById(my_test_id)
-})
-
-app.get('/all-instances', async (req, res) => {
-    var allInstances = [
-
-    ]
-
-    for(const instance of ec2Instances) {
-        instanceInfo = {}
-
-        instanceInfo.Id = instance
-        instanceInfo.Name = await getEC2Name(instance)
-
-        allInstances.push(instanceInfo)
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-
-    res.send(allInstances)
-})
-
-app.get('/test', async (req, res) => {
-    res.send("AWSMCStart backend API test")
-    console.log(await getEC2Name(my_test_id))
-})
-
-app.listen(port, () => {
-    console.log(`AWSMCStart backend API running on port ${port}`)
-    startEC2StopDaemon()
-})
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var trpc_1 = require("./trpc");
+var standalone_1 = require("@trpc/server/adapters/standalone");
+var appRouter = (0, trpc_1.router)({
+    userList: trpc_1.publicProcedure
+        .query(function () { return __awaiter(void 0, void 0, void 0, function () {
+        var users;
+        return __generator(this, function (_a) {
+            users = ["test1", "test2", "test3"];
+            console.log("Request recieved");
+            return [2 /*return*/, users];
+        });
+    }); }),
+});
+var server = (0, standalone_1.createHTTPServer)({
+    router: appRouter,
+});
+server.listen(3000);
