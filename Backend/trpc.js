29b9a3b901ff207870c49973e4c8abdc3f860606
Blake <80996688+BlakeTnr@users.noreply.github.com>
2024-08-09 21:40:51 -0400

    Reinitalize tsconfig
    Re-initalized because I had just copied it over from the frontend

--- Backend/trpc.js
+++ Backend/trpc.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.publicProcedure = exports.router = void 0;
-var server_1 = require("@trpc/server");
+const server_1 = require("@trpc/server");
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-var t = server_1.initTRPC.create();
+const t = server_1.initTRPC.create();
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
