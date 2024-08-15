7d7c1a71dd108af61ee3a8f8fc726d1fa5fa7914
Blake <80996688+BlakeTnr@users.noreply.github.com>
2024-08-14 21:04:17 -0400

    Add start instance api

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,13 +1,22 @@
 import { publicProcedure, router } from './trpc';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import { getAllInstancesInfo, tagEC2Instance } from './ec2Utils';
+import { getAllInstancesInfo, startEC2StopDaemon, startInstanceById, tagEC2DelayedStop, tagEC2Instance } from './ec2Utils';
 import cors from 'cors';
+import { z } from 'zod';
  
 const appRouter = router({
     ec2Instances: publicProcedure
       .query(async () => {
         return await getAllInstancesInfo()
       }),
+    startEC2Instance: publicProcedure
+      .input(z.string())
+      .mutation(async (opts) => {
+        const { input } = opts;
+
+        await tagEC2DelayedStop(input, 1600)
+        return await startInstanceById(input)
+      })
   },
   );
 
